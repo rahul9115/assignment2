42ffbb7d55d8f207d93dd7dec9a75abcbdaa8506
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2021-08-24 18:34:42 +0530

    validation_done

--- check.py
+++ check.py
@@ -5,15 +5,21 @@ def mysqlconnect():
         host='localhost',
         user='root', 
         password = "rahul9115",
-        db='assignment',
+        db='assignment2',
         )
       
     cur = conn.cursor()
     #print(f"insert into user_information(name,age,stream,gender) values('{name}',{age},'{stream}','{gender}');")
-    cur.execute("select * from user_information")
+    cur.execute("select username,password from administrator")
     #cur.execute(f"insert into user_information(name,age,stream,gender) values('{name}',{age},'{stream}','{gender}');")
     output = cur.fetchall()
     print(output)
+    user=[]
+    passwd=[]
+    for i in output:
+        user.append(i[0])
+        passwd.append(i[1])
+    print(user,passwd)
       
     # To close the connection
     conn.close()
--- store.py
+++ store.py
@@ -5,6 +5,45 @@ app=Flask(__name__,template_folder="sign_up")
 @app.route("/",methods=["POST","GET"])
 def display():
     return render_template("validate.html")
+@app.route("/validate",methods=["POST","GET"])
+def validate():
+    print("in")
+    if request.method=="POST":
+        username=request.form.get("username")
+        password=request.form.get("password")
+        print(username,password)
+        conn = pymysql.connect(
+        host='localhost',
+        user='root', 
+        password = "rahul9115",
+        db='assignment2',
+        )
+      
+        cur = conn.cursor()
+        #print(f"insert into user_information(name,age,stream,gender) values('{name}',{age},'{stream}','{gender}');")
+        cur.execute("select username,password from administrator")
+        #cur.execute(f"insert into user_information(name,age,stream,gender) values('{name}',{age},'{stream}','{gender}');")
+        output = cur.fetchall()
+        user=[]
+        passwd=[]
+        
+        for i in output:
+            user.append(i[0])
+            passwd.append(i[1])
+        u=False
+        p=False
+        print(user,passwd)
+        for i,j in zip(user,passwd):
+            if(i==username):
+                u=True
+            if(j==password):
+                p=True
+        print(u,p)
+        if(u==True and p==True):
+            return render_template("info.html")
+        else:
+            return render_template("validate.html")
+
 @app.route("/info.html",methods=["POST","GET"])
 
 def insert():
@@ -40,7 +79,7 @@ def insert():
 
 
     # To connect MySQL database
-         
+ 
     
   
 # Driver Code
